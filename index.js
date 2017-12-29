c2aaf78b846378c506609a9a9af1baef0e9fd522
XRayV5 <ruixu0530@gmail.com>
2017-12-30 00:31:41 +1100

    inital commit

---
+++ index.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: 'eslint-config-airbnb',
+  rules: {
+    semi: [2, 'never'],
+    singleQuote: true,
+    'no-unused-vars': ['warn', { vars: 'local', args: 'none' }],
+    'max-params': [2, 5],
+    'max-statements': [2, 50],
+    jsxBracketSameLine: true,
+    firstAttributeOnSameLine: true,
+    'react/prop-types': 0,
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'react/jsx-no-bind': 'warn',
+  },
+  env: {
+    browser: true,
+    es6: true,
+  },
+}
+
